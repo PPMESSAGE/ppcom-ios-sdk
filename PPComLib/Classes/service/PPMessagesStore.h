eb3fad404a0c46dac600d824bfa211ab2945cf9a
Jin He <jin.he@ppmessage.com>
2016-07-07 13:10:56 +0800

    update PPMessageStore

---
+++ PPComLib/Classes/service/PPMessagesStore.h
@@ -0,0 +1,31 @@
+//
+//  PPMessageStore.h
+//  Pods
+//
+//  Created by Jin He on 7/7/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "PPMessage.h"
+
+@class PPSDK, PPMessage;
+
+@interface PPMessagesStore : NSObject
+
+@property (nonatomic) PPSDK *sdk;
+@property (nonatomic) NSMutableDictionary *messages;
+
++ (instancetype)storeWithClient:(PPSDK*)client;
+
+- (NSMutableArray*)messagesInCovnersation:(NSString*)conversationUUID;
+
+- (NSMutableArray*)messagesInCovnersation:(NSString *)conversationUUID autoCreate:(BOOL)autoCreate;
+
+- (void)setMessageList:(NSMutableArray*)messageList forConversation:(NSString*)conversationUUID;
+
+- (BOOL)updateWithNewMessage:(PPMessage*)message;
+
+- (void)updateMessageStatus:(PPMessageStatus)status messageIndentifier:(NSString *)identifier conversationUUID:(NSString *)conversationUUID;
+
+@end
