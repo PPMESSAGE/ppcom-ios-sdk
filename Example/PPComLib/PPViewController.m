3e50006d2283f7ffec3a5c659e97086af1ce7a36
Jin He <jin.he@ppmessage.com>
2016-07-08 19:33:31 +0800

    add 2 configuration for development use

--- Example/PPComLib/PPViewController.m
+++ Example/PPComLib/PPViewController.m
@@ -22,11 +22,18 @@
     
     // Configure PPSDK
     PPSDKConfiguration *sdkConfiguration = [[PPSDKConfiguration alloc] init];
-    sdkConfiguration.hostUrl = @"http://192.168.0.204:8945";
-    sdkConfiguration.apiKey = @"YjRlZTYyNWY3ZThjMDJlNDg3YjRkYjNkZDQzNTk0NjdmODk1ZTMzNg==";
-    sdkConfiguration.apiSecret = @"MTU3ZWE3MWQ4MTc0NzgxNjRhNGViMTdhMWUyMDUxZTRlYzAzNjg2YQ==";
-    sdkConfiguration.appUUID = @"9c60acbd-44bb-11e6-94c0-acbc327f19e9";
-//    sdkConfiguration.email = @"test_user@gamil.com";
+    // sdkConfiguration.hostUrl = @"http://192.168.0.204:8945";
+    // sdkConfiguration.apiKey = @"YjRlZTYyNWY3ZThjMDJlNDg3YjRkYjNkZDQzNTk0NjdmODk1ZTMzNg==";
+    // sdkConfiguration.apiSecret = @"MTU3ZWE3MWQ4MTc0NzgxNjRhNGViMTdhMWUyMDUxZTRlYzAzNjg2YQ==";
+    // sdkConfiguration.appUUID = @"9c60acbd-44bb-11e6-94c0-acbc327f19e9";
+    // sdkConfiguration.email = @"test_user@gamil.com";
+    
+    sdkConfiguration.hostUrl = @"http://192.168.0.52:8945";
+    sdkConfiguration.apiKey = @"ZGEwYjExYzFkOGI1N2YxYTRiNGI5MGYzMzQwZjQ3NTllZmY2MWMzNw==";
+    sdkConfiguration.apiSecret = @"NTEyYmJhOWM3NTZhYWRlNjhhYjA1ODljODU0YjlhODUwZDViZDAyYw==";
+    sdkConfiguration.appUUID = @"6892a540-4439-11e6-98b2-ac87a30c6610";
+    sdkConfiguration.email = @"test_user@gamil.com";
+    
     [[PPSDK sharedSDK] configure:sdkConfiguration];
     
     // Start PPSDK
