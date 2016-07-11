359df4b95dcb52e10304f542ab151a465280c654
Guijin Ding <dingguijin@gmail.com>
2016-07-11 16:41:46 +0800

    pod.sh to create a tagged pod

---
+++ pod.sh
@@ -0,0 +1,12 @@
+tag=$1
+
+git tag -a "$1" -m "tag $1" 
+
+git push --tags
+
+pod spec lint PPComLib.podspec
+
+pod trunk push PPComLib.podspec
+
+#pod trunk register guijin.ding@ppmessage.com 'Guijin Ding' --description='macbook pro 17inch'
+#pod trunk add-owner PPComLib guijin.ding@ppmessage.com
