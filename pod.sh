a5439cb3837e9d598787f968fb39119a064ba28f
Jin He <jin.he@ppmessage.com>
2016-07-27 17:28:48 +0800

    update pod.sh

--- pod.sh
+++ pod.sh
@@ -1,3 +1,9 @@
+# REMEMBER to modify version number in PPComLib.podspec and commit changes before add tag !
+
+# If you pushed a wrong tag to github, you can delete that tag using following commands:
+#   git tag -d tagName
+#   git push origin :refs/tags/tagName
+
 tag=$1
 
 git tag -a "$1" -m "tag $1" 
