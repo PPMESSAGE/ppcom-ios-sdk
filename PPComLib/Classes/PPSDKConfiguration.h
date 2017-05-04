5cff47f57ad65b5ebcd6e041322e684bb15efcc7
Guijin Ding <dingguijin@gmail.com>
2017-05-04 18:04:47 +0800

    fix @ string

--- PPComLib/Classes/PPSDKConfiguration.h
+++ PPComLib/Classes/PPSDKConfiguration.h
@@ -20,17 +20,16 @@
 @property (nonatomic, readwrite) NSString *uploadUrl;
 @property (nonatomic, readwrite) NSString *authUrl;
 
-@property (nonatomic) NSString *apiSecret = "ZThmMTM1ZDM4ZmI2NjE1YWE0NWEwMGM3OGNkMzY5MzVjOTQ2MGU0NQ==";
-@property (nonatomic) NSString *apiKey = "M2E2OTRjZTQ5Mzk4ZWUxYzRjM2FlZDM2NmE4MjA4MzkzZjFjYWQyOA==";
+@property (nonatomic) NSString *apiSecret = @"ZThmMTM1ZDM4ZmI2NjE1YWE0NWEwMGM3OGNkMzY5MzVjOTQ2MGU0NQ==";
+@property (nonatomic) NSString *apiKey = @"M2E2OTRjZTQ5Mzk4ZWUxYzRjM2FlZDM2NmE4MjA4MzkzZjFjYWQyOA==";
 
 /** 
  * Set configuration of PPCOM SDK
  * @author Guijin Ding
  *
- * @param hostUrl, https://ppmessage.cn or https://ppmessage.com, depends on your account which registered in.
- * @param appUUID, your team app uuid, get it from PPMESSAGE backend
- * @param regisgrationId, iOS app push id, if no, can not send push from PPMESSAGE
- * @return void
+ * @param hostUrl https://ppmessage.cn or https://ppmessage.com, depends on your account which registered in.
+ * @param appUUID your team app uuid, get it from PPMESSAGE backend
+ * @param regisgrationId iOS app push id, if no, can not send push from PPMESSAGE 
  */
 - (void)setHostUrl:(NSString *)hostUrl appUUID:(NSString*)appUUID registrationId:(NSString*)registrationId;
 
